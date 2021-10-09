53ed6736abc474b894cc376521dc4d682a38e85c
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-10-08 19:24:07 -0700

    update translate.py

--- assignments/05_proteins/translate.py
+++ assignments/05_proteins/translate.py
@@ -25,8 +25,8 @@ def get_args():
                         help='A file with codon translations',
                         required=True,
                         metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
+                        type=argparse.FileType('rt'))
+    # since required=True, doesn't need default=None
 
     parser.add_argument('-o',
                         '--outfile',
@@ -45,22 +45,29 @@ def main():
     args = get_args()
     codon_table = {}
     for line in args.codons:
-        codon = line.rstrip().split()[0]
-        aa = line.rstrip().split()[1]
+        # codon = line.rstrip().split()[0]
+        # aa = line.rstrip().split()[1]
+        codon, aa = line.split()
+        # put both things into 2 placeholders
+        # but this might break if you have three things on a line
+        # no need to use rstrip() because once it splits at the new line
+        # just throw it away because nothing is after that
         codon_table[codon] = aa
 
     k = 3
     seq = args.sequence
     out = ''
     for codon in [seq[i:i + k] for i in range(0, len(seq), k)]:
-        if codon.upper() in codon_table:
-            aa = codon_table.get(codon.upper())
-        else:
-            aa = '-'
-        out += aa
+        # if codon.upper() in codon_table:
+        #     aa = codon_table.get(codon.upper())
+        # else:
+        #     aa = '-'
+        # out += aa
+        out += codon_table.get(codon.upper(), '-')
 
-    args.outfile.write(out)
-    args.outfile.close()
+    # args.outfile.write(out)
+    # args.outfile.close()
+    print(out, file=args.outfile)
     print(f'Output written to "{args.outfile.name}".')
 
 
