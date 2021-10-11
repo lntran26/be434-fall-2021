5465e12b453f5a449fe2daf781db76b9c730772a
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-10-11 10:25:42 -0700

    create common.py

---
+++ assignments/06_common/common.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Author : lntran <lntran@localhost>
+Date   : 2021-10-08
+Purpose: Find common words
+"""
+
+import argparse
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Find common words',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file1',
+                        metavar='FILE1',
+                        help='Input file 1',
+                        type=argparse.FileType('rt'))
+
+    parser.add_argument('file2',
+                        metavar='FILE2',
+                        help='Input file 2',
+                        type=argparse.FileType('rt'))
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        metavar='FILE',
+                        help='Output file',
+                        type=argparse.FileType('wt'),
+                        default=sys.stdout)
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Main program"""
+
+    args = get_args()
+
+    file1 = args.file1.read().split()
+    file2 = args.file2.read().split()
+    out = []
+
+    for word in file1:
+        if word in file2:
+            out.append(word)
+
+    for word in sorted(set(out)):
+        print(word, file=args.outfile)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
