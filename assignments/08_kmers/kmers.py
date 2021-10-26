73981eb8ea62ccab6e27f2b9eefc2888de293e98
Linh Tran <38679445+lntran26@users.noreply.github.com>
2021-10-26 09:31:53 -0700

    Update kmers.py

--- assignments/08_kmers/kmers.py
+++ assignments/08_kmers/kmers.py
@@ -62,40 +62,59 @@ def test_find_kmers():
     assert find_kmers('ACTG', 3) == ['ACT', 'CTG']
     assert find_kmers('ACTG', 4) == ['ACTG']
     assert find_kmers('ACTG', 5) == []
+
 # --------------------------------------------------
 
 
-def count_kmers(word_list, k):
+def count_kmers(words_list, k):
     """ Count k-mers in a list of strings (words)
     Return a dictionary with k-mers as keys and counts as values"""
-    words_dict = {}
-    for word in word_list:
+    kmers_dict = {}
+    for word in words_list:
         for kmer in find_kmers(word, k):
-            if kmer not in words_dict:
-                words_dict[kmer] = 1
+            if kmer not in kmers_dict:
+                kmers_dict[kmer] = 1
             else:
-                words_dict[kmer] += 1
-    return words_dict
+                kmers_dict[kmer] += 1
+    return kmers_dict
+
+# --------------------------------------------------
+
+
+def test_count_kmers():
+    """ Test count_kmers """
+    assert count_kmers([], 1) == {}
+    assert count_kmers(['ACTG', 'TGGG'], 5) == {}
+    assert count_kmers(['AACTTGAC'], 1) == {'A': 3, 'C': 2, 'T': 2, 'G': 1}
+    assert count_kmers(['foo', 'bar', 'baz'], 3) == {
+        'foo': 1, 'bar': 1, 'baz': 1}
+    assert count_kmers(['foo', 'bar', 'baz'], 2) == {
+        'fo': 1, 'oo': 1, 'ba': 2, 'ar': 1, 'az': 1}
+    assert count_kmers(['quux', 'bar', 'flip', 'foo'], 4) == {
+        'quux': 1, 'flip': 1}
 
 # --------------------------------------------------
 
 
 def main():
-    """Make a jazz noise here"""
+    """Main program"""
 
     args = get_args()
 
-    file1 = args.file1.read().split()
-    file2 = args.file2.read().split()
+    words_list1 = args.file1.read().split()
+    words_list2 = args.file2.read().split()
 
-    dict1 = count_kmers(file1, args.kmer)
-    dict2 = count_kmers(file2, args.kmer)
+    kmers_dict1 = count_kmers(words_list1, args.kmer)
+    kmers_dict2 = count_kmers(words_list2, args.kmer)
 
-    unique_keys = set(list(dict1.keys())+list(dict2.keys()))
+    # unique_keys = set(list(kmers_dict1.keys())+list(kmers_dict2.keys()))
+    unique_keys = set(kmers_dict1.keys()).union(set(kmers_dict2.keys()))
 
     for key in unique_keys:
-        if key in dict1 and key in dict2:
-            print('{:<10}{:>6}{:>6}'.format(key, dict1[key], dict2[key]))
+        if key in kmers_dict1 and key in kmers_dict2:
+            # print('{:<10}{:>6}{:>6}'.format(
+            #     key, kmers_dict1[key], kmers_dict2[key]))
+            print(f'{key:10}{kmers_dict1[key]:6}{kmers_dict2[key]:6}')
 
 
 # --------------------------------------------------
